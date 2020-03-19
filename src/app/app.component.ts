4826f4d4878082e96d72b7aa45f6550e5bb702ce
shrinivas <shrinivas.kattimani@digital.datamatics.com>
2020-03-19 15:51:11 +0530

    facebook logon code added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'chidambara-rahasya';
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    this.fbLibrary();
+  }
+
+  fbLibrary() {
+    (window as any).fbAsyncInit = function() {
+      window['FB'].init({
+        appId: '869805000070130',
+        cookie: true,
+        xfbml: true,
+        version: 'v3.1'
+      });
+      window['FB'].AppEvents.logPageView();
+    };
+
+    (function(d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = 'https://connect.facebook.net/en_US/sdk.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, 'script', 'facebook-jssdk');
+  }
+
+  login() {
+    window['FB'].login(
+      response => {
+        console.log('login response', response);
+        if (response.authResponse) {
+          window['FB'].api(
+            '/me',
+            {
+              fields: 'last_name, first_name, email'
+            },
+            userInfo => {
+              console.log('user information');
+              console.log(userInfo);
+            }
+          );
+        } else {
+          console.log('User login failed');
+        }
+      },
+      { scope: 'email' }
+    );
+  }
 }
